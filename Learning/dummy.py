d967d59a48a5ace1bd7ee67370a2a7bbc8a4959c
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2023-08-11 23:22:28 +0530

    dummy class work

--- Learning/dummy.py
+++ Learning/dummy.py
@@ -1,1 +1,25 @@
-print("hello ")
+class Client:
+
+    def __init__(self,id,name):   
+        self.clientId=id
+        self.clientName=name
+
+    
+class Payment(Client):
+    def __init__(self,id,name,pId,amount):
+        super().__init__(id,name) 
+        self.paymentId= pId
+        self.paymentAmount= amount
+
+   
+    
+    def display(self):
+        print("Client ID      : ",self.clientId)
+        print("Client Name    : ",self.clientName)
+        print("Payment ID     : ",self.paymentId)
+        print("Payment Amount : ",self.paymentAmount)
+
+
+p1=Payment(101,"Rohan",122,3000)
+# print(p1.getclientId)
+p1.display()
