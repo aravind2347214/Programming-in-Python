7a9d6770695e089f33e257907e5f872dddc32bf1
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2023-08-21 15:50:04 +0530

    re practivce

--- Learning/dummy.py
+++ Learning/dummy.py
@@ -1,25 +1,15 @@
-class Client:
-
-    def __init__(self,id,name):   
-        self.clientId=id
-        self.clientName=name
-
-    
-class Payment(Client):
-    def __init__(self,id,name,pId,amount):
-        super().__init__(id,name) 
-        self.paymentId= pId
-        self.paymentAmount= amount
-
-   
-    
-    def display(self):
-        print("Client ID      : ",self.clientId)
-        print("Client Name    : ",self.clientName)
-        print("Payment ID     : ",self.paymentId)
-        print("Payment Amount : ",self.paymentAmount)
+# 
 
 
-p1=Payment(101,"Rohan",122,3000)
-# print(p1.getclientId)
-p1.display()
+
+BLACKWHITE=[['black'],['white','black'],['white'],['white','black'],['white'],['black']]
+
+def uniqueCount(colorList):
+    color_dict={}
+    for node in colorList:
+        color_dict[tuple(node)]= colorList.count(node)
+    print(color_dict)  
+
+uniqueCount(BLACKWHITE) 
+
+
